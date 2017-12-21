1c647152a1375398566261b0427e3632a4f2a362
Melissa <meliarneau@gmail.com>
2017-12-21 17:48:02 +0000

    Added quiz directions for 2-11

--- lesson11/quiz2-11.js
+++ lesson11/quiz2-11.js
@@ -1,4 +1,15 @@
 /*
+
+Directions:
+Mad Libs is a word game where players have fun substituting words for blanks in a story. For this exercise, use the adjective variables below to fill in the blanks and complete the following message.
+
+"The Intro to JavaScript course is __________. James and Julia are so __________. I cannot wait to work through the rest of this __________ content!"
+var adjective1 = "amazing";
+var adjective2 = "fun";
+var adjective3 = "entertaining";
+Assign the resulting string to a variable called madLib.
+
+
  * Programming Quiz: MadLibs (2-11)
  * 
  * 1. Declare a madLib variable
