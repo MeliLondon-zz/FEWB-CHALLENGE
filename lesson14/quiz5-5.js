25c408b1bbdacdd7bdda30f11c9368671c3fdc94
Melissa <meliarneau@gmail.com>
2018-01-11 11:20:32 +0000

    Created JS quiz Cry 5-5

---
+++ lesson14/quiz5-5.js
@@ -0,0 +1,10 @@
+/* Directions:
+Write a named function expression that stores the function in a variable called cry and returns "boohoo!". Don't forget to call the function using the variable name, not the function name:
+
+cry(); */
+
+var cry = function emotion() {
+  return "boohoo!";
+};
+
+console.log( cry() );
