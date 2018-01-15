9b7f3e95b448b856a56c87a7475f932390a225a2
Melissa <meliarneau@gmail.com>
2018-01-15 18:00:54 +0000

    Created JS quiz The Price Is Right 6-3

---
+++ lesson15/quiz6-3.js
@@ -0,0 +1,15 @@
+/* Directions:
+Starting with this array of prices, change the prices of the 1st, 3rd, and 7th elements in the array.
+
+var prices = [1.23, 48.11, 90.11, 8.50, 9.99, 1.00, 1.10, 67.00];
+TIP: The 1st element of any array has an index of 0.
+
+Afterwards, print out the prices array to the console. */
+
+var prices = [1.23, 48.11, 90.11, 8.50, 9.99, 1.00, 1.10, 67.00];
+
+prices[1] = 22.84;
+prices[3] = 28.28;
+prices[7] = 44.22;
+
+console.log(prices);
