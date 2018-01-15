fff15a3ac835da1ef6f67174eb6ffd7a3f91e467
Melissa <meliarneau@gmail.com>
2018-01-15 17:49:46 +0000

    Created JS quiz UdaciFamily 6-1

---
+++ lesson15/quiz6-1.js
@@ -0,0 +1,8 @@
+/* Directions:
+Create an array called udaciFamily and add "Julia", "James", and your name to the array. Then, print the udaciFamily to the console using console.log. */
+
+var udaciFamily = ["Julia","James","Melissa"];
+
+console.log(udaciFamily[0]);
+console.log(udaciFamily[1]);
+console.log(udaciFamily[2]);
