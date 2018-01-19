2320cfdbe1c3ee065d3e69ac22d43184b38e212d
Melissa <meliarneau@gmail.com>
2018-01-19 13:14:19 +0000

    Created JS quiz String Equality for All 2-4

---
+++ lesson2/quiz2-4.js
@@ -0,0 +1,7 @@
+/* Directions:
+Fix the right side expression so it evaluates to true.
+
+"ALL Strings are CrEaTeD equal" == "All STRINGS are CrEaTED Equal" */
+
+var answer = "ALL Strings are CrEaTeD equal" == "ALL Strings are CrEaTeD equal";
+console.log(answer);
