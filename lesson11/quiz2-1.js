6fa3aa5e815121b113d5f412a6f78f0ed937f0a3
Melissa <meliarneau@gmail.com>
2018-01-19 13:20:25 +0000

    Created JS quiz First Expression 2-1

---
+++ lesson11/quiz2-1.js
@@ -0,0 +1,6 @@
+/* Directions:
+Write an expression that uses at least 3 different arithmetic operators.
+The expression should equal 42.
+Hint: +, -, *, /, and % are possible arithmetic operators */
+
+console.log(((12 + 2) * 6) /2);
