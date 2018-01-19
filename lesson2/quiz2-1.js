5175797d23d999e5f1850b83cddea4596d7afdee
Melissa <meliarneau@gmail.com>
2018-01-19 13:20:47 +0000

    Copied quiz into lesson 11

--- lesson2/quiz2-1.js
+++
@@ -1,8 +0,0 @@
-/* Directions:
-Write an expression that uses at least 3 different arithmetic operators.
-
-The expression should equal 42.
-
-Hint: +, -, *, /, and % are possible arithmetic operators */
-
-console.log(((12 + 2) * 6) /2);
