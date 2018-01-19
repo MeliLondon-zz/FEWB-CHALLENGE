05c5b23a81205e7f1bca0d66820a42a8284be017
Melissa <meliarneau@gmail.com>
2018-01-19 13:23:02 +0000

    Copied into lesson 11

--- lesson2/quiz2-3.js
+++
@@ -1,6 +0,0 @@
-/*  Directions:
-Create a string with the name of your favorite food. The first letter of the string should be capitalized. */
-
-var favouriteFood = "Chocolate";
-
-console.log(favouriteFood);
